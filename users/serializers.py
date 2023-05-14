5ef4b29108d23924f6f325eec91233b731666869
HamzaKhan001 <spaceus9@gmail.com>
2023-05-14 19:59:16 +0500

    user model data serialized

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+
+class UserSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(min_length=8,write_only=True)
+    
+    
+    class Meta:
+        model=User
+        fields=('username','email','password')
+        
