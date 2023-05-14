49d2d6c4e5c4d621971ea68fd0d37d2afd5e1f0b
HamzaKhan001 <spaceus9@gmail.com>
2023-05-14 22:12:05 +0500

    changes in settings/urls

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from users.urls import urls as users_urls
+from users import urls as users_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
